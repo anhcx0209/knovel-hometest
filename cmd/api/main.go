036d843c522ac9e199e1472c12978a92900ed074
cxa <anhcx0209@gmail.com>
2024-12-15 01:13:50 +0700

    use echo as http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/anhcx0209/knovel-hometest/config"
 	"github.com/anhcx0209/knovel-hometest/database"
-	internal "github.com/anhcx0209/knovel-hometest/internal/http/gin"
+	internal "github.com/anhcx0209/knovel-hometest/internal/http/echo"
 )
 
 func main() {
 	conf := config.GetConfig()
 	db := database.NewPostgresDatabase(conf)
-	// server := internal.NewEchoServer(conf, db)
-	server := internal.NewGinServer(conf, db)
+	server := internal.NewEchoServer(conf, db)
+	// server := internal.NewGinServer(conf, db)
 	server.Start()
 }
